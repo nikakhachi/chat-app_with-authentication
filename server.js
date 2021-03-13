f40ac1212668bde4a391f10aa32cc36285066f86
Nika Khachiashvili <o.sicknick@gmail.com>
2021-03-13 14:28:58 +0400

    adding build for heroku

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ mongoose.connect(
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/private', require('./routes/api/private'));
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT : ${PORT}`))
