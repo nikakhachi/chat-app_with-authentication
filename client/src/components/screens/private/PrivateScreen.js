42c04bebc1a8501dd62fd60025d68e45e102fff6
Nika Khachiashvili <o.sicknick@gmail.com>
2021-03-13 13:34:26 +0400

    adding styles to privatescreen

--- client/src/components/screens/private/PrivateScreen.js
+++ client/src/components/screens/private/PrivateScreen.js
@@ -1,27 +1,20 @@
 import { Link } from 'react-router-dom'; 
-import { useState } from 'react';
+import './privateScreen.css'
 
 function PrivateScreen(props){
-
-    const [img, setImg] = useState(undefined);
 
     function logOut(){
         localStorage.removeItem("authToken");
     }
 
-    console.log(img);
-
     return (
-        <div>
+        <div id='private-screen'>
+            <h1 id='private-title'>Welcome To Private Route</h1>
+            <h2 id='private-description'>User Registration Log</h2>
             {props.data.map((item, index) => (
-                <p key={index} style={{fontSize: '1.5vw'}}>{item.username} Joined in {item.register_date.replace('T', ' ').slice(0, 16)} UTC Time</p>
+                <p className='private-log' key={index}>{item.username} Joined in {item.register_date.replace('T', ' ').slice(0, 16)} UTC Time</p>
             ))}
-            <button style={{fontSize: '1.5vw', marginTop: '2%'}} onClick={logOut}><Link to='/login'>Log Out</Link></button>
-            <input 
-                type="file" 
-                accept=".png, .jpg, .jpeg"
-                onChange={(e) => setImg(e.target.files[0])}
-            />
+            <Link id='log-out-link' to='/login'><button id='log-out-btn' onClick={logOut}>Log Out</button></Link>
         </div>
     )
 }
