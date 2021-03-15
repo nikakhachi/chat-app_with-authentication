41c1170cd895100f410e28037c20ab599c3277f0
Nika Khachiashvili <o.sicknick@gmail.com>
2021-03-15 16:30:42 +0400

    adding source code component

---
+++ client/src/components/source_code/sourceCode.js
@@ -0,0 +1,12 @@
+import './sourceCode.css'
+
+function SourceCode(){
+    return (
+        <a id='source-link' href='https://github.com/xnick7x/mern-authentication' 
+            rel="noreferrer" target="_blank">
+                <i className="fa fa-github" aria-hidden="true"></i> <span></span>
+        </a>
+    )
+}
+
+export default SourceCode;
