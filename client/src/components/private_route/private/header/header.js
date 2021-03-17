f0bde15d29e10791540ed2856d22f1f7f118c4a9
Nika Khachiashvili <o.sicknick@gmail.com>
2021-03-17 18:11:29 +0400

    adding content and styling header

--- client/src/components/private_route/private/header/header.js
+++ client/src/components/private_route/private/header/header.js
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 import { authenticateFailure } from '../../../../redux/auth/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
+import Popup from 'reactjs-popup';
 
 function Header(){
+
+    const user = useSelector(state => state.auth_data.user);
 
     const dispatch = useDispatch();
 
@@ -15,8 +18,13 @@ function Header(){
 
     return (
         <header id='private-header'>
-            under construction
-            <Link id='log-out-link' to='/login'><button id='log-out-btn' onClick={logOut}>Log Out</button></Link>
+            <p id='private-header-title' className='private-header-item'><i className="fa fa-commenting" aria-hidden="true"></i> Chat Room</p>
+            <p id='account-settings' className='private-header-item'>Account Settings</p>
+            <Popup modal trigger={<i className="private-header-item fa fa-cog" aria-hidden="true"></i>} position="right center">
+                <>
+                </>
+            </Popup>
+            <Link className='private-header-item' id='log-out-link' to='/login'><button id='log-out-btn' onClick={logOut}>Log Out</button></Link>
         </header>
     )
 }
